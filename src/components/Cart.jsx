6a664f9b517ce3bcfcc6a18f47a4ff6c239957aa
Nahuel Medina <nahuelmedina738@gmail.com>
2023-12-03 16:32:31 -0300

    working on final proyecxt

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,16 +4,16 @@ import { CartContext } from "../contexts/CartContext"
 export const Cart = () => {
 
     const { items, clear } = useContext(CartContext)
+    const total = items.reduce((accumulator, currentValue) => accumulator + (currentValue.quantity * currentValue.price), 0)
 
-    // if (!!items.length) {
-    //     return <div>Sin elementos</div>
-    // }
     return (
         <div>
             <h1>Cart</h1>
+            <h2>total : {total}</h2>
             {items?.map(item => <div key={item.id}>
                 <h2>{item.name}</h2>
-                <h3>{item.price}</h3>
+                <h3>cantidad: {item.quantity}</h3>
+                <h3>precio: ${item.price}</h3>
                 <img src={item.imageUrl} alt={item.name} width={300} />
             </div>)}
             <button onClick={clear}>Vaciar carrito</button>
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,14 +1,21 @@
 import { Link } from 'react-router-dom'
 import cart from '../../assets/cart.svg'
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/CartContext'
 import './CartWidget.css'
 
 
 export const CartWidget = () => {
+
+    const { items } = useContext(CartContext)
+    const total = items.reduce((accumulator, currentValue) => accumulator + currentValue.quantity, 0)
     return (
-        <Link to="/cart">
-            <img className='cartImg' src={cart} alt="carrito" />
-            <span>0</span>
-        </Link>
+        <div>
+            <Link to="/cart">
+                <img className='cartImg' src={cart} alt="carrito" />
+                <span>{total}</span>
+            </Link>
+        </div>
 
     )
 }
